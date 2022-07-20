825948000f7acb089bc43611fe43143be5697fc9
Lucas Neuhaus <lucas.neuhaus93@gmail.com>
2022-07-19 23:35:38 -0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import TheButton from "./components/TheButton.vue";
+</script>
+
+<template>
+  <TheButton>Not Selected</TheButton>
+  <TheButton selected>Selected</TheButton>
+</template>
---
+++ src/components/TheButton.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+const props = defineProps({
+  selected: Boolean,
+});
+</script>
+
+<template>
+  <button :class="{ active: props.selected }"><slot /></button>
+</template>
+
+<style scoped>
+button {
+  border: 1px solid blue;
+  border-radius: 999px;
+  padding: 12px;
+}
+
+button.active {
+  color: blue;
+}
+
+button:hover {
+  cursor: pointer;
+}
+</style>
